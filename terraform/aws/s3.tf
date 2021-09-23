02c43fa845ad30831577d3a0984f7552d871e1c8
amarkovich <amarkovich@paloaltonetworks.com>
2021-09-23 16:30:57 +0300

    test1

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -4,8 +4,6 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
-  acl           = "public-read"
-  force_destroy = true
   tags = merge({
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
@@ -16,7 +14,6 @@ resource "aws_s3_bucket" "data" {
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
     yor_trace            = "0874007d-903a-4b4c-945f-c9c233e13243"
   })
 }
@@ -91,15 +88,14 @@ resource "aws_s3_bucket" "operations" {
 resource "aws_s3_bucket" "data_science" {
   # bucket is not encrypted
   bucket = "${local.resource_prefix.value}-data-science"
-  acl    = "private"
   versioning {
-    enabled = true
+    enabled = false
   }
   logging {
     target_bucket = "${aws_s3_bucket.logs.id}"
     target_prefix = "log/"
   }
-  force_destroy = true
+  force_destroy = false
   tags = {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/s3.tf"
