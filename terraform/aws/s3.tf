1ae93c5453a688bb66bfa51aa003956558369d30
amitmarkovich92 <90555895+amitmarkovich92@users.noreply.github.com>
2021-11-08 16:42:26 +0200

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -21,7 +21,7 @@ resource "aws_s3_bucket" "data" {
   })
 }
 
-resource "aws_s3_bucket" "kuku" {
+resource "aws_s3_bucket" "kuku123" {
   # bucket is public
   # bucket is not encrypted
   # bucket does not have access logs
